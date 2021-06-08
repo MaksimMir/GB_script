e12e794db298c343184c50e08cc23904eca9686c
MaxM <mak-miroshnichen@yandex.ru>
2021-06-08 16:11:21 +0300

    task 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,18 @@
+import product from './components/product';
+import CreateCard from './components/createcard';
+
 window.addEventListener('load', () => {
-    let title = document.createElement('h1');
-    title.innerText = 'Hello World';
-    document.body.append(title);
+    'use strict';
+
+    new CreateCard(product, '.product_list', 6).init();
+
+    const btns = document.querySelectorAll('.black_link');
+    for (const el of btns) {
+        el.addEventListener('click', (evt) => {
+            const id = evt.target.dataset.id;
+            console.log(product[id]);
+        })
+    }
+    
+
 })
